4c0d404f30ff802809e1c8039eaa6e607f61b9a8
Rob Lokhorst <roblokhorst@gmail.com>
2014-11-29 01:30:07 +0100

    WIP

--- seeingspaces.js
+++ seeingspaces.js
@@ -7,51 +7,77 @@ Runs = new Mongo.Collection("runs");
 
 if (Meteor.isClient) {
 
-  Template.controlSurface.helpers({
-    screen: [1,2,3,4]
+  Meteor.subscribe('runs_ready', function(){
+    var active = Session.get("active");
+    var newest = getNewest();
+
+    if (active == null && newest) {
+      Session.set("active", newest._id);
+    }
   });
 
   Template.remote1.events({
     "submit .new-run": function (event) {
-      // This function is called when the new run form is submitted
       var text = event.target.text.value;
       if (text == "") return false;
-
-      Runs.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
-
-      // Clear form
+      Runs.insert({ text: text, createdAt: new Date() });
       event.target.text.value = "";
-
-      // Prevent default form submit
       return false;
     }
   });
 
   Template.remote2.helpers({
+
+    currentTimestamp: function() {
+      return Session.get("timestamp")
+    },
     runs: function () {
       return Runs.find({}, {sort: {createdAt: -1}});
+    },
+    startTime: function() {
+      var currentRun = Runs.findOne(Session.get("active"));
+      return currentRun.createdAt.getTime();
+    },
+    endTime: function() {
+      var currentRun = Runs.findOne(Session.get("active"));
+      var startTime = currentRun.createdAt.getTime();
+      var nextRun = Runs.findOne({createdAt: {$gt: new Date(startTime)}}, {sort: {createdAt: 1}});
+      return nextRun.createdAt.getTime();
+    },
+    prettyTime: function() {
+      return moment(currentTime).format('H:mm:ss');
+    },
+    percentage: function() {
+      return 0;
     }
   });
 
+  function getNewest() {
+    return Runs.findOne({}, {sort: {createdAt: -1}});
+  }
+
   Template.remote2.events({
-    "click tr": function () {
-      var active = InMemory.findOne({ key: "active" });
-      if (active)
-        InMemory.update(active._id, {$set: {value: this._id}});
-      else
-        InMemory.insert({ key: "active", value: this._id });
+    "click tr": function() {
+      if (getNewest()._id == this._id) console.log('laatste');
+      Session.set("active", this._id);
+      //Session.set("timestamp", getStartTimestamp(this._id));
+    },
+    "change .currentTime": function() {
+      //console.log(currentTime);
     }
   });
 
   Template.run.helpers({
     createdAt: function() {
-      return moment(this.createdAt).fromNow();
+      moment.locale('nl-NL');
+      return moment(this.createdAt).format('dd H:mm:ss');
+    },
+    timestamp: function() {
+      return this.createdAt.getTime();
     },
     maybeActive: function() {
-      return InMemory.findOne({ key: "active", value: this._id });
+
+      return Session.get("active") == this._id;
     }
   });
 }
@@ -59,5 +85,9 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+
+  Meteor.publish('runs_ready', function(){
+    return Runs.find({});
   });
 }
